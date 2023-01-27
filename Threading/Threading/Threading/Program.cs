c060ddcb18ae68de9e3d70a490047d82c94f7a62
Piotr Kowalski <piotrkow93@gmail.com>
2023-01-27 15:53:57 +0100

    BackgroudThread

---
+++ Threading/Threading/Threading/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.NetworkInformation;
+
+
+Console.WriteLine("Hello, World!");
+var bgThread = new Thread(() =>
+{
+    while (true)
+    {
+        bool isNetworkUp = NetworkInterface.GetIsNetworkAvailable();
+
+        Console.WriteLine($"Is network avaiable: {isNetworkUp}");
+        Thread.Sleep(1000);
+    }
+});
+
+bgThread.IsBackground = true;
+bgThread.Start();
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Main thread is working...");
+    Task.Delay(500);
+}
+
+Console.WriteLine("Done");
+Console.ReadKey();
