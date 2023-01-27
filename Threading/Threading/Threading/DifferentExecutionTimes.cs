988dac451326c3d83f5e8e93f39f8a74f440af99
Piotr Kowalski <piotrkow93@gmail.com>
2023-01-27 16:31:58 +0100

    Exceptions

---
+++ Threading/Threading/Threading/DifferentExecutionTimes.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.NetworkInformation;
+
+namespace Threading
+{
+    internal class DifferentExecutionTimes
+    {
+        public void Main()
+        {
+            var bgThread = new Thread((object? data) =>
+            {
+                if (data is null) return;
+                int counter = 0;
+                var result = int.TryParse(data.ToString(), out int maxCount);
+
+                if (!result) return;
+                while (counter < maxCount)
+                {
+                    bool isNetworkUp = NetworkInterface.GetIsNetworkAvailable();
+
+                    Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} | Is network avaiable: {isNetworkUp} | {counter}");
+                    Thread.Sleep(10);
+                    counter++;
+                }
+            });
+
+            bgThread.Start(12);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} | Main thread is working...");
+                Thread.Sleep(400);
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+}
--- Threading/Threading/Threading/Program.cs
+++ Threading/Threading/Threading/Program.cs
@@ -1,32 +1,7 @@
 ﻿using System.Net.NetworkInformation;
 
-// Passing Parameter to thread
-var bgThread = new Thread((object? data) =>
-{
-    if (data is null) return;
-    int counter = 0;
-    var result = int.TryParse(data.ToString(), out int maxCount);
 
-    if (!result) return;
-    while (counter < maxCount)
-    {
-        bool isNetworkUp = NetworkInterface.GetIsNetworkAvailable();
+//ThreadStartException - problem with thread before running code
+//ThreadStateException - metod is not avaiable in the current state
+//ThreadAbortException - Abort is not possible in .net core and above
 
-        // Thread.CurrentThread.ManagedThreadId Thread unique Id
-        Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} | Is network avaiable: {isNetworkUp} | {counter}");
-        Thread.Sleep(10);
-        counter++;
-    }
-});
-
-// Passing Parameter to thread
-bgThread.Start(12);
-
-for (int i = 0; i < 10; i++)
-{
-    Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} | Main thread is working...");
-    Thread.Sleep(400);
-}
-
-Console.WriteLine("Done");
-Console.ReadKey();
