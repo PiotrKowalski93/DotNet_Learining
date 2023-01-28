950e7355969585620aa3f8eaf9c8563d13342421
Piotr Kowalski <piotrkow93@gmail.com>
2023-01-28 11:34:40 +0100

    Mutex

--- Threading/Threading/Threading/Program.cs
+++ Threading/Threading/Threading/Program.cs
@@ -1,7 +1,3 @@
 ﻿using System.Net.NetworkInformation;
 
 
-//ThreadStartException - problem with thread before running code
-//ThreadStateException - metod is not avaiable in the current state
-//ThreadAbortException - Abort is not possible in .net core and above
-
