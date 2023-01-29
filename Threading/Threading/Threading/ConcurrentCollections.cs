bc81eb2c37730d4ebc352093c3caf02f13706096
Piotr Kowalski <piotrkow93@gmail.com>
2023-01-29 17:29:24 +0100

    Concurrent Collection Basics

---
+++ Threading/Threading/Threading/ConcurrentCollections.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Concurrent;
+
+namespace Threading
+{
+    public class ConcurrentCollections
+    {
+        // most common collections in concurrent programming
+        ConcurrentBag<int> bag = new ConcurrentBag<int>();
+        ConcurrentQueue<int> queue = new ConcurrentQueue<int>();
+        ConcurrentStack<int> stack = new ConcurrentStack<int>();
+        BlockingCollection<int> blockingCollection = new BlockingCollection<int>();
+        ConcurrentDictionary<int, string> dict = new ConcurrentDictionary<int, string>();
+
+
+    }
+}
