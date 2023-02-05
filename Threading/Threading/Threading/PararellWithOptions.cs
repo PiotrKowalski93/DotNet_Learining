6eb11ae70372db3dbe67a9508215341370167cf6
Piotr Kowalski <piotrkow93@gmail.com>
2023-02-05 18:48:32 +0100

    Pararell

---
+++ Threading/Threading/Threading/PararellWithOptions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    internal class PararellWithOptions
+    {
+        public void Main()
+        {
+
+        }
+
+        public bool PlinqWithLimits(List<string> items)
+        {
+            int max = Environment.ProcessorCount > 1 ? Environment.ProcessorCount / 2 : 1;
+            return items.AsParallel().WithDegreeOfParallelism(max).Any(x => x.Contains("sth"));
+        }
+
+        public void ProcessParallelForEachWithLimits(List<string> items)
+        {
+            int max = Environment.ProcessorCount > 1 ? Environment.ProcessorCount / 2 : 1;
+
+            var options = new ParallelOptions()
+            {
+                MaxDegreeOfParallelism = max
+            };
+
+            Parallel.ForEach(items, options, x =>
+            {
+                // process items
+            });
+        }
+    }
+}
--- Threading/Threading/Threading/Program.cs
+++ Threading/Threading/Threading/Program.cs
@@ -2,8 +2,5 @@
 using System.Net.NetworkInformation;
 using Threading;
 
-ConcurrentBag<int> bag = new ConcurrentBag<int>();
-ConcurrentQueue<int> queue = new ConcurrentQueue<int>();
-ConcurrentStack<int> stack = new ConcurrentStack<int>();
 
 Console.ReadKey();
